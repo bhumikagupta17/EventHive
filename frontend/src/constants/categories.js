d24073be27345a3a7f14c88e1a0b855b4c54943f
Bhumika <bhumika17gupta@gmail.com>
2026-08-19 09:22:58 +0530

    set up axios api client and category constants:

---
+++ frontend/src/constants/categories.js
@@ -0,0 +1,30 @@
+export const EVENT_CATEGORIES = [
+  'Technology',
+  'Arts & Culture',
+  'Athletics',
+  'Workshop',
+  'Regional',
+  'Social',
+  'Academic'
+]
+
+export const CATEGORY_FILTER_OPTIONS = [
+  'All categories',
+  ...EVENT_CATEGORIES
+]
+
+export const STORAGE_KEYS = {
+  EVENTS: 'eventhive_events',
+  REGISTRATIONS: 'eventhive_registrations',
+  USER: 'eventhive_user'
+}
+
+export const CAMPUS_BUILDINGS = [
+  'CSH',
+  'Student Activity Center-204',
+  'Student Activity Center-206',
+  'VCH',
+  'Central Lawn',
+  'WE-1',
+  'WE-2',
+];
